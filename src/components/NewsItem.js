dee2b50f74ecb0f08b90557af84e82312bfd24e7
Anmol Ranjan <anmol@appvintech.com>
2025-01-05 19:50:02 +0530

    CHanges

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
   render() {
     return (
-      <div>NewsItem</div>
+      <div>NewsItem
+        ff99b46517834d58bab9cdf4ec7cd643
+      </div>
     )
   }
 }
